52e1625e4c8ffcb2aaef25ef5848d25de3bf3cf2
TermuxGuide <64996709+TermuxGuide@users.noreply.github.com>
2020-11-18 18:01:41 +0600

    Add files via upload

---
+++ vkN.py
@@ -0,0 +1,14 @@
+from vk import *
+import random, vk, time
+token = "токен (VK ADMIN)"
+user_id = "ацдин страницы" 
+posts_id = "айди поста"
+msgs = input("Введите сообщение: ")
+session = vk.Session(access_token=token)
+apivk = vk.API(session, v = 5.95)
+while True:
+    try:
+        print(apivk.wall.createComment(owner_id=user_id, post_id=posts_id, message=msgs, guid=random.randint(0, 9999999999)))
+    except:
+        pass
+    time.sleep(3)
